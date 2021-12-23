852a0f42266a97025c8af4f692d05f225fd36081
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-23 16:46:13 +0530

    month with if else if stmt

---
+++ Month2.java
@@ -0,0 +1,44 @@
+class Month2{
+	public static void main(String[] ag){
+	
+		int id = 1;
+		if(id == 1){
+			System.out.println("JAN");
+		} else if(id == 1){
+			System.out.println("JAN");
+		}
+		else if(id == 2){
+			System.out.println("FEB");
+		}
+		else if(id == 3){
+			System.out.println("MAR");
+		}
+		else if(id == 4){
+			System.out.println("APR");
+		}
+		else if(id == 5){
+			System.out.println("MAY");
+		}
+		else if(id == 6){
+			System.out.println("JUN");
+		}
+		else if(id == 7){
+			System.out.println("JUL");
+		}
+		else if(id == 8){
+			System.out.println("AUG");
+		}
+		else if(id == 9){
+			System.out.println("SEP");
+		}
+		else if(id == 10){
+			System.out.println("OCT");
+		}
+		else if(id == 11){
+			System.out.println("SEP");
+		}
+		else if(id == 12){
+			System.out.println("DEC");
+		}
+	}
+}
