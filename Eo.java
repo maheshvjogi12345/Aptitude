7df2ce754ba6b8544143700dbc9c476be74b7735
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-23 16:43:22 +0530

    using for and if

---
+++ Eo.java
@@ -0,0 +1,11 @@
+class Eo{
+	public static void main(String[] ar){
+		int num = 10;
+		if(num%2 == 0){
+			System.out.println("even");
+			}else{
+				System.out.println("odd");	
+				}
+			}
+		}
+	
