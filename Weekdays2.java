9281016a124d405f3bb22d2fd85b2409a2bf2b5c
unknown <maheshvjogi.xworkz@gmail.com git config --global user.email maheshvjogi.xworkz@gmail.com>
2021-12-23 16:47:35 +0530

    days with if else if stmt

---
+++ Weekdays2.java
@@ -0,0 +1,29 @@
+class Weekdays2{
+	public static void main(String[] am){
+	
+		int days = 7;
+		if(days == 1){
+			System.out.println("SUN");
+			}
+			else if(days == 2){
+			System.out.println("MON");
+			}
+			else if(days == 3){
+			System.out.println("TUE");
+			}
+			else if(days == 4){
+			System.out.println("WED");
+			}
+			else if(days == 5){
+			System.out.println("THUR");
+			}
+			else if(days == 6){
+			System.out.println("FRI");
+			}
+			else if(days == 7){
+			System.out.println("SAT");
+			}
+		}
+}
+			
+			
